0f790ffa06921a26e3ea43305ab2e82c89319f2d
emelehova <103140191+emelehova@users.noreply.github.com>
2025-11-03 00:55:50 +0300

    Initialize AppDelegate with main application window

---
+++ AppDelegate.swift
@@ -0,0 +1,27 @@
+import AppKit
+import WebKit
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var window: NSWindow!
+    var controller: BrowserViewController!
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        let rect = NSRect(x: 0, y: 0, width: 1200, height: 800)
+        window = NSWindow(
+            contentRect: rect,
+            styleMask: [.titled, .closable, .miniaturizable, .resizable],
+            backing: .buffered,
+            defer: false
+        )
+        window.title = "FantasyBorwser"
+        controller = BrowserViewController()
+        window.contentViewController = controller
+        window.center()
+        window.makeKeyAndOrderFront(nil)
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        true
+    }
+}
